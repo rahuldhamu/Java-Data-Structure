11834de884ca4c0afb4dcca350c9ebe26315bf69
Rahul Kumar Dhamu <dhamu.31954@gmail.com>
2018-10-01 13:33:26 +0530

    Update Linkkist.java

--- Linkkist.java
+++ Linkkist.java
@@ -1,1 +1,20 @@
 
+class node {
+  private int info;
+  private node next;
+  node(int info){
+    this.info = info;
+    this.next = null;
+  }
+  
+  public int get_info(){
+    return this.info;
+  }
+  public node next(){
+    return this.next;
+  }
+  public void update_info(int new_info){
+    this.info = new_info;
+  }
+  
+}
